2d09f34a60e19eea2fcf3bee4c674b7c4ba9c5f1
jaydaiya1512 <jaydaiya1512@gmail.com>
2025-02-01 22:26:12 +0530

    uploaded code files

---
+++ script.js
@@ -0,0 +1,15 @@
+const allowedPasswords = ["7001", "7004", "7024", "7026", "7030", "7040"];
+const passwordInput = document.getElementById("password");
+const loginButton = document.getElementById("login-btn");
+const errorMsg = document.getElementById("error-msg");
+
+loginButton.addEventListener("click", () => {
+  const password = passwordInput.value;
+  if (allowedPasswords.includes(password)) {
+    window.location.href = "study.html"; // Redirect to study materials page
+  } else {
+    errorMsg.textContent = "Incorrect password 😏! You cannot access!";
+  }
+});
+
+
