af92f3c14cb213f677ec9b7838aba77469688927
jaydaiya1512 <jaydaiya1512@gmail.com>
2025-02-16 17:08:39 +0530

    UPDATED CODE FOR ACCESS BTN

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const allowedPasswords = ["7001", "7004", "7024", "7026", "7030", "7040"];
 const passwordInput = document.getElementById("password");
 const loginButton = document.getElementById("login-btn");
 const errorMsg = document.getElementById("error-msg");
+const accessButton = document.getElementById("access-btn");
 
 loginButton.addEventListener("click", () => {
   const password = passwordInput.value;
@@ -10,6 +11,12 @@ loginButton.addEventListener("click", () => {
   } else {
     errorMsg.textContent = "Incorrect password 😏! You cannot access!";
   }
+});
+
+accessButton.addEventListener("click", () => {
+  const phoneNumber = "7861838882"; // Replace with actual WhatsApp number
+  const message = encodeURIComponent("I want access 🥹");
+  window.location.href = `https://wa.me/${phoneNumber}?text=${message}`;
 });
 
 function goBack() {
